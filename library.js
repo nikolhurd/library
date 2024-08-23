a46ec5e433d107ae1e228e1e625ad2f4884d4a62
Nikol <nikolhurd@gmail.com>
2024-08-23 20:30:19 +0200

    created books and add them to myLibrary

--- library.js
+++ library.js
@@ -10,3 +10,13 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(newBook) {
   myLibrary.push(newBook);
 }
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+const Dune = new Book("Dune", "Frank Herbert", 412, "not read yet");
+const AtomicHabbit = new Book("Atomic Habits", "James Clear", 306, "read");
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(Dune);
+addBookToLibrary(AtomicHabbit);
+
+console.log(myLibrary);
