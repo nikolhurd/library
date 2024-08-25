e337aa1dc5963a2faf68012f40722712b1d83566
Nikol <nikolhurd@gmail.com>
2024-08-25 21:35:17 +0200

    JS creating elements for book card

--- library.js
+++ library.js
@@ -19,4 +19,27 @@ addBookToLibrary(theHobbit);
 addBookToLibrary(Dune);
 addBookToLibrary(AtomicHabbit);
 
-const btnAdd = document.getElementById(btnAdd);
+const btnAdd = document.querySelector("#btnAdd");
+const container = document.querySelector(".container");
+
+const bookTitle = document.createElement("h3");
+bookTitle.setAttribute("class", "book-title");
+container.append(bookTitle);
+
+const bookAuthor = document.createElement("p");
+bookAuthor.setAttribute("class", "book-author");
+container.append(bookAuthor);
+
+const bookPages = document.createElement("p");
+bookPages.setAttribute("class", "book-pages");
+container.append(bookPages);
+
+const btnRead = document.createElement("p");
+btnRead.setAttribute("id", "btn-read");
+container.append(btnRead);
+
+function displayLibrary(myLibrary) {
+  myLibrary.forEach((e) => {
+    e;
+  });
+}
